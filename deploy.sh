0a8503273f5aabf8fbeec7e923a9b25a9888b274
Juergen Etzlstorfer <7677092+jetzlstorfer@users.noreply.github.com>
2019-03-06 16:29:35 +0100

    fix dockerfile

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-REGISTRY_URL=$(kubectl describe svc docker-registry -n cicd | grep IP: | sed 's~IP:[ \t]*~~')
-CHANNEL_URI=$1
+REGISTRY_URI=$(kubectl describe svc docker-registry -n keptn | grep IP: | sed 's~IP:[ \t]*~~')
 
 rm -f config/gen/servicenow-operator.yaml
 
 cat config/servicenow-operator.yaml | \
-  sed 's~CHANNEL_URI_PLACEHOLDER~'"$CHANNEL_URI"'~' | \
   sed 's~REGISTRY_URI_PLACEHOLDER~'"$REGISTRY_URI"'~' >> config/gen/servicenow-operator.yaml 
-  
+
+kubectl delete -f config/gen/servicenow-operator.yaml
+
 kubectl apply -f config/gen/servicenow-operator.yaml
