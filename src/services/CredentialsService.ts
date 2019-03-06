96f43b0bce0d3a39d26364961dec2e236752db74
Juergen Etzlstorfer <7677092+jetzlstorfer@users.noreply.github.com>
2019-03-06 11:03:17 +0100

    add dyntrace credentials

--- src/services/CredentialsService.ts
+++ src/services/CredentialsService.ts
@@ -3,6 +3,7 @@ import { K8sClientFactory } from '../lib/K8sClientFactory';
 import * as K8sApi from 'kubernetes-client';
 
 import { base64encode, base64decode } from 'nodejs-base64';
+import { DynatraceCredentials } from '../types/DynatraceCredentials';
 
 export class CredentialsService {
 
@@ -32,16 +33,36 @@ export class CredentialsService {
       .get({ name: 'servicenow', pretty: true, exact: true, export: true });
 
     if (s.body.items && s.body.items.length > 0) {
-      const ghItem = s.body.items.find(item => item.metadata.name === 'servicenow');
-      if (ghItem && ghItem.data !== undefined) {
-        // console.log(ghItem.data);
-        serviceNowCredentials.tenant = base64decode(ghItem.data.tenant);
-        serviceNowCredentials.user = base64decode(ghItem.data.user);
-        serviceNowCredentials.token = base64decode(ghItem.data.token);
+      const secretItem = s.body.items.find(item => item.metadata.name === 'servicenow');
+      if (secretItem && secretItem.data !== undefined) {
+        serviceNowCredentials.tenant = base64decode(secretItem.data.tenant);
+        serviceNowCredentials.user = base64decode(secretItem.data.user);
+        serviceNowCredentials.token = base64decode(secretItem.data.token);
       }
     }
 
     return serviceNowCredentials;
+  }
+
+  async getDynatraceCredentials(): Promise<DynatraceCredentials> {
+    const dynatraceCredentials: DynatraceCredentials = {
+      tenant: '',
+      token: '',
+    };
+
+    const s = await this.k8sClient.api.v1
+      .namespaces('keptn').secrets
+      .get({ name: 'dynatrace', pretty: true, exact: true, export: true });
+
+    if (s.body.items && s.body.items.length > 0) {
+      const secretItem = s.body.items.find(item => item.metadata.name === 'dynatrace');
+      if (secretItem && secretItem.data !== undefined) {
+        dynatraceCredentials.tenant = base64decode(secretItem.data.DT_TENANT_ID);
+        dynatraceCredentials.token = base64decode(secretItem.data.DT_API_TOKEN);
+      }
+    }
+
+    return dynatraceCredentials;
   }
 
 }
