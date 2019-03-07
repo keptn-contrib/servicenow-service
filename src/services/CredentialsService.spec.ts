331a0844f23189ba2e7889604b96d331aff6808d
Juergen Etzlstorfer <7677092+jetzlstorfer@users.noreply.github.com>
2019-03-07 09:18:06 +0100

    refactoring

--- src/services/CredentialsService.spec.ts
+++ src/services/CredentialsService.spec.ts
@@ -2,12 +2,10 @@ import 'reflect-metadata';
 import * as express from 'express';
 import { expect } from 'chai';
 import 'mocha';
-import * as sinon from 'sinon';
 import { cleanUpMetadata } from 'inversify-express-utils';
 import { CredentialsService  } from './CredentialsService';
 import { ServiceNowCredentials } from '../types/ServiceNowCredentials';
 import { DynatraceCredentials } from '../types/DynatraceCredentials';
-const nock = require('nock');
 
 describe('CredentialsService', () => {
   let credService: CredentialsService;
@@ -30,30 +28,4 @@ describe('CredentialsService', () => {
     expect(dtCreds.token).to.be.a('string');
   }).timeout(5000);
 
-  // it('Should not create a K8s secret - credentials undefined', async () => {
-  //   const newGitCreds : CredentialsModel = undefined;
-  //   const updated : boolean = await credService.updateGithubConfig(newGitCreds);
-  //   expect(updated).to.be.false;
-  // }).timeout(5000);
-
-  // it('Should not create a K8s secret - missing parameter', async () => {
-  //   const newGitCreds = new CredentialsModel();
-  //   newGitCreds.org = 'keptn-org';
-  //   const updated = await credService.updateGithubConfig(newGitCreds);
-  //   expect(updated).to.be.false;
-  // }).timeout(5000);
-
-  // it('Should create a K8s secret', async () => {
-  //   const gitCreds : CredentialsModel = await credService.getGithubCredentials();
-  //   const newGitCreds = new CredentialsModel();
-  //   newGitCreds.org = 'keptn-org';
-  //   newGitCreds.user = 'keptn';
-  //   //newGitCreds.token='abc123';
-  //   const updated = await credService.updateGithubConfig(newGitCreds);
-  //   expect(updated).to.be.false;
-
-  //   if (updated) {
-  //     await credService.updateGithubConfig(gitCreds);
-  //   }
-  // });
 });
