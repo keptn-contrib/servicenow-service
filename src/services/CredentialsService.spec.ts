22b17eccbd7afd9d2ace042a08b0d6e782952e70
Juergen Etzlstorfer <7677092+jetzlstorfer@users.noreply.github.com>
2019-03-11 16:53:35 +0100

    typo

--- src/services/CredentialsService.spec.ts
+++ src/services/CredentialsService.spec.ts
@@ -22,7 +22,7 @@ describe('CredentialsService', () => {
     expect(snowCreds.token).to.be.a('string');
   }).timeout(5000);
 
-  it('Should return the Dyantrace credentials from a K8s secret', async () => {
+  it('Should return the Dynatrace credentials from a K8s secret', async () => {
     const dtCreds : DynatraceCredentials = await credService.getDynatraceCredentials();
     expect(dtCreds.tenant).to.be.a('string');
     expect(dtCreds.token).to.be.a('string');
