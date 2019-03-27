715ff26864dbb01090a1c8e0ec6bc626064f18fe
Juergen Etzlstorfer <7677092+jetzlstorfer@users.noreply.github.com>
2019-03-27 15:04:26 +0100

    rename operator to service

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 REGISTRY_URI=$(kubectl describe svc docker-registry -n keptn | grep IP: | sed 's~IP:[ \t]*~~')
 
-rm -f config/gen/servicenow-operator.yaml
+rm -f config/gen/servicenow-service.yaml
 
-cat config/servicenow-operator.yaml | \
-  sed 's~REGISTRY_URI_PLACEHOLDER~'"$REGISTRY_URI"'~' >> config/gen/servicenow-operator.yaml 
+cat config/servicenow-service.yaml | \
+  sed 's~REGISTRY_URI_PLACEHOLDER~'"$REGISTRY_URI"'~' >> config/gen/servicenow-service.yaml 
 
-kubectl delete -f config/gen/servicenow-operator.yaml
+kubectl delete -f config/gen/servicenow-service.yaml
 
-kubectl apply -f config/gen/servicenow-operator.yaml
+kubectl apply -f config/gen/servicenow-service.yaml
